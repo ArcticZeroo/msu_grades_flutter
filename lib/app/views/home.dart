764a0cde0f13555d2e5369707ba16c4a9a582d17
Olivia Mikola <t-olmiko@microsoft.com>
2020-07-28 13:28:15 -0400

    Added skeleton class for Search delegate

--- lib/app/views/home.dart
+++ lib/app/views/home.dart
@@ -10,10 +10,14 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text("Title goes here")),
-      body: Center(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+      appBar: AppBar(title: Text("MSU Grades")),
+      body: Center(
+        child: FlatButton(
+          textColor: Colors.green, // Theme.color.of(context).primaryTextTheme.bodyText2,
+            child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 1), borderRadius: BorderRadius.circular(6)),
+                child: Text("Search MSU Grades", textScaleFactor: 4.0,))),
       ),
     );
   }
