459be97b7586a2626b31b5df08dbb9ea6980110b
Spencer <spencer@frozor.io>
2020-07-28 23:21:52 -0400

    Add course.id getter which combines codes

--- lib/api/data/grades/course.dart
+++ lib/api/data/grades/course.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:msu_grades/api/data/grades/course_term.dart';
 import 'package:msu_grades/api/data/grades/msu_object.dart';
 
-class Course extends MsuObject{
+class Course extends MsuObject {
   /// Title of the course i.e. Introduction to python
   String title;
 
@@ -15,8 +15,13 @@ class Course extends MsuObject{
   String description;
   List<CourseTerm> terms;
 
+  /// Combination of subject code and course code as a getter, e.g. CSE 231
+  String get id {
+    return '$subjectCode $courseCode';
+  }
+
   @override
-  displayDuringLookup(){
+  displayDuringLookup() {
     return FlatButton(child: Text("$subjectCode $courseCode $title"));
   }
 }
